3ef9e06dce4ffc3c1ad4cd3e4fcdffc30d59182e
akshaysolanki941 <42600205+akshaysolanki941@users.noreply.github.com>
2020-10-11 09:56:35 +0530

    Update code.py

--- code.py
+++ code.py
@@ -8,6 +8,132 @@ import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS
 from nltk.corpus import stopwords
 
+CONTRACTION_MAP = {
+	"ain't": "is not",
+	"aren't": "are not",
+	"can't": "cannot",
+	"can't've": "cannot have",
+	"'cause": "because",
+	"could've": "could have",
+	"couldn't": "could not",
+	"couldn't've": "could not have",
+	"didn't": "did not",
+	"doesn't": "does not",
+	"don't": "do not",
+	"hadn't": "had not",
+	"hadn't've": "had not have",
+	"hasn't": "has not",
+	"haven't": "have not",
+	"he'd": "he would",
+	"he'd've": "he would have",
+	"he'll": "he will",
+	"he'll've": "he he will have",
+	"he's": "he is",
+	"how'd": "how did",
+	"how'd'y": "how do you",
+	"how'll": "how will",
+	"how's": "how is",
+	"I'd": "I would",
+	"I'd've": "I would have",
+	"I'll": "I will",
+	"I'll've": "I will have",
+	"I'm": "I am",
+	"I've": "I have",
+	"i'd": "i would",
+	"i'd've": "i would have",
+	"i'll": "i will",
+	"i'll've": "i will have",
+	"i'm": "i am",
+	"i've": "i have",
+	"isn't": "is not",
+	"it'd": "it would",
+	"it'd've": "it would have",
+	"it'll": "it will",
+	"it'll've": "it will have",
+	"it's": "it is",
+	"let's": "let us",
+	"ma'am": "madam",
+	"mayn't": "may not",
+	"might've": "might have",
+	"mightn't": "might not",
+	"mightn't've": "might not have",
+	"must've": "must have",
+	"mustn't": "must not",
+	"mustn't've": "must not have",
+	"needn't": "need not",
+	"needn't've": "need not have",
+	"o'clock": "of the clock",
+	"oughtn't": "ought not",
+	"oughtn't've": "ought not have",
+	"shan't": "shall not",
+	"sha'n't": "shall not",
+	"shan't've": "shall not have",
+	"she'd": "she would",
+	"she'd've": "she would have",
+	"she'll": "she will",
+	"she'll've": "she will have",
+	"she's": "she is",
+	"should've": "should have",
+	"shouldn't": "should not",
+	"shouldn't've": "should not have",
+	"so've": "so have",
+	"so's": "so as",
+	"that'd": "that would",
+	"that'd've": "that would have",
+	"that's": "that is",
+	"there'd": "there would",
+	"there'd've": "there would have",
+	"there's": "there is",
+	"they'd": "they would",
+	"they'd've": "they would have",
+	"they'll": "they will",
+	"they'll've": "they will have",
+	"they're": "they are",
+	"they've": "they have",
+	"to've": "to have",
+	"wasn't": "was not",
+	"we'd": "we would",
+	"we'd've": "we would have",
+	"we'll": "we will",
+	"we'll've": "we will have",
+	"we're": "we are",
+	"we've": "we have",
+	"weren't": "were not",
+	"what'll": "what will",
+	"what'll've": "what will have",
+	"what're": "what are",
+	"what's": "what is",
+	"what've": "what have",
+	"when's": "when is",
+	"when've": "when have",
+	"where'd": "where did",
+	"where's": "where is",
+	"where've": "where have",
+	"who'll": "who will",
+	"who'll've": "who will have",
+	"who's": "who is",
+	"who've": "who have",
+	"why's": "why is",
+	"why've": "why have",
+	"will've": "will have",
+	"won't": "will not",
+	"won't've": "will not have",
+	"would've": "would have",
+	"wouldn't": "would not",
+	"wouldn't've": "would not have",
+	"y'all": "you all",
+	"y'all'd": "you all would",
+	"y'all'd've": "you all would have",
+	"y'all're": "you all are",
+	"y'all've": "you all have",
+	"you'd": "you would",
+	"you'd've": "you would have",
+	"you'll": "you will",
+	"you'll've": "you will have",
+	"you're": "you are",
+	"you've": "you have"
+}
+
 def plot(X,Y,xlabel,ylabel,title):
 	plt.bar(X, Y, tick_label = X, width = 0.8, color = ['red', 'green'])
 	plt.xlabel(xlabel)
@@ -67,6 +193,10 @@ data1 = re.sub(r'CHAPTER \d+', '', data1)
 data1 = re.sub('[\(\[].*?[\)\]]', '', data1)
 data1 = re.sub(r'[^a-zA-Z0-9\s]', '', data1)
 data1 = data1.replace('\n', '')
+contractions_re = re.compile('(%s)' % '|'.join(CONTRACTION_MAP.keys()))
+def replace(match):
+    return CONTRACTION_MAP[match.group(0)]
+data1 = contractions_re.sub(replace, data1)
 
 # pre-processing the text of Book-2
 data2 = data2.lower()
@@ -76,6 +206,10 @@ data2 = re.sub(r'CHAPTER \d+', '', data2)
 data2 = re.sub('[\(\[].*?[\)\]]', '', data2)
 data2 = re.sub(r'[^a-zA-Z0-9\s]', '', data2)
 data2 = data2.replace('\n', '')
+contractions_re = re.compile('(%s)' % '|'.join(CONTRACTION_MAP.keys()))
+def replace(match):
+    return CONTRACTION_MAP[match.group(0)]
+data2 = contractions_re.sub(replace, data2)
 
 # tokenizing
 token1 = nltk.word_tokenize(data1)
